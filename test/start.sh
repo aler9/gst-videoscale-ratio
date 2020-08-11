2ce369bcfab17648891d59678fb6121fceb7f4e9
aler9 <46489434+aler9@users.noreply.github.com>
2020-08-11 15:40:10 +0200

    cleanup

--- test/start.sh
+++ test/start.sh
@@ -1,22 +1,7 @@
 #!/bin/sh -e
 
-#! videoscale add-borders=1 ! video/x-raw,width=1280,height=720
-#x264enc speed-preset=veryfast tune=zerolatency bitrate=500 \
-#! rtspclientsink location=rtsp://localhost:8554/stream
-#videotestsrc ! video/x-raw,width=320,height=320
-#! rtspclientsink location=rtsp://localhost:8554/stream
-#! videoscaleratio height=720 \
-
-#videotestsrc ! video/x-raw,width=1280,height=720
-#! capsfilter caps=video/x-raw,width=400,height=400
-#
-#rtph264depay \
-#! h264parse \
-
 gst-launch-1.0 \
-rtspsrc location=rtsp://localhost:8554/stream \
-! "application/x-rtp,media=(string)video" \
-! decodebin \
+videotestsrc ! video/x-raw,width=1280,height=720 \
 ! videoscaleratio height=320 \
 ! x264enc speed-preset=veryfast tune=zerolatency \
 ! decodebin \
